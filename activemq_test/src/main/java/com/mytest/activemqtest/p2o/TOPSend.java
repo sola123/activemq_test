5a487583a706891e11182a8b5fc8d63873a7241e
sola123 <34559708+sola123@users.noreply.github.com>
2018-06-22 15:24:05 +0800

    小更改
    rt

--- activemq_test/src/main/java/com/mytest/activemqtest/p2o/TOPSend.java
+++ activemq_test/src/main/java/com/mytest/activemqtest/p2o/TOPSend.java
@@ -1,7 +1,6 @@
 package com.mytest.activemqtest.p2o;
 
 import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
@@ -10,7 +9,6 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import com.mytest.activemqtest.ConnectRes;
-import org.apache.activemq.ActiveMQConnectionFactory;
 
 /**
  * 发布和订阅
